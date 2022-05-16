9fcdfa23c0a35d670a479e9061ac6b0aec808898
praduman <praduman1417@gmail.com>
2022-05-16 21:41:03 +0530

    added terraform files and modifyied workflow

---
+++ s3.tf
@@ -0,0 +1,5 @@
+resource "aws_s3_bucket" "msp-client-s3-bucket" {
+     bucket =  "msp-client-terraform-state"
+     key    = "client/terraform.tfstate"
+
+}
---
+++ state.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3"{
+      bucket = "msp-oboarding-terraform-state"
+      key= "onboarding/terraform.state"
+      region = "us-east-1"
+         
+  }
+}
