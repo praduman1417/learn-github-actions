baf7cdef81bcffc4d8ea2cbd4712042076f32f79
praduman <praduman1417@gmail.com>
2022-05-16 21:47:03 +0530

    modterraform files

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "msp-client-s3-bucket" {
      bucket =  "msp-client-terraform-state"
-     key    = "client/terraform.tfstate"
+    /// key    = "client/terraform.tfstate"
 
 }
