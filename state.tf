9a9f34620b24208714d5837abb9a6a6e70d2f71f
praduman <praduman1417@gmail.com>
2022-05-16 21:50:01 +0530

    mod terraform files

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3"{
       bucket = "msp-oboarding-terraform-state"
-      //key= "onboarding/terraform.state"
+      key = "onboarding/terraform.state"
       region = "us-east-1"
          
   }
